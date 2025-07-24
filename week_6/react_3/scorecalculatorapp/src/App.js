77ef3d2d23699c101c0deffe0307f0b50ebc43a5
Mohammed-AsrarAhmed <mohammedasrar.ahmed04@gmail.com>
2025-07-24 14:10:05 +0530

    react work

---
+++ week_6/react_3/scorecalculatorapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { CalculateScore } from './Components/CalculateScore';
+function App() {
+  return (
+    <div>
+     <CalculateScore Name={'Asrar'}
+     School={'MVSR School'}
+     total={284}
+     goal={3}
+     />
+    </div>
+  );
+}
+
+export default App;
